391b7c39efdc61b3519bdafb98bc531a9b7d2c3b
Malcolm Jones <bossjones@theblacktonystark.com>
2024-01-21 18:45:17 -0500

    chg: rancher

--- scripts/install.sh
+++ scripts/install.sh
@@ -1,25 +1,25 @@
 #!/usr/bin/env bash
 
-cd ~/dev/bossjones/k3d-playground || exit
+cd ~/dev/bossjones/k3d-playground
 
-cd vendor/local-chats/charts/echoserver || exit
-make install
-cd - || exit
-
-cd vendor/local-chats/charts/kube-prometheus-stack || exit
-make install
-cd - || exit
-
-cd vendor/local-chats/charts/loki-distributed || exit
-make install
-cd - || exit
-
-cd vendor/local-chats/charts/promtail || exit
-make install
-cd - || exit
-
-cd vendor/local-chats/charts/ranc || exither
+cd vendor/local-chats/charts/echoserver
 make install
 cd -
 
-cd ~/dev/bossjones/k3d-playground
+cd vendor/local-chats/charts/kube-prometheus-stack
+make install
+cd -
+
+cd vendor/local-chats/charts/loki-distributed
+make install
+cd - || exit
+
+cd vendor/local-chats/charts/promtail
+make install
+cd -
+
+cd vendor/local-chats/charts/rancher
+make install
+cd - || exit
+
+cd ~/dev/bossjones/k3d-playground || exit
