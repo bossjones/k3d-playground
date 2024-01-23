9e8b8186adbc57cbd23afc50d848414024370d2f
Malcolm Jones <bossjones@theblacktonystark.com>
2024-01-22 19:27:25 -0500

    chg: rdy to run

--- scripts/argocd-password.sh
+++ scripts/argocd-password.sh
@@ -3,19 +3,7 @@ set -euxo pipefail
 
 kubectx k3d-demo
 
-export GIT_URI=$(git config --get remote.origin.url | sed -e 's/:/\//g' | sed -e 's/ssh\/\/\///g' | sed -e 's/git@/https:\/\//g' | sed 's/.git$//')
-
-kubectl apply -f - <<EOF
-apiVersion: v1
-kind: Secret
-metadata:
-  name: main-repository
-  namespace: argocd
-  labels:
-    argocd.argoproj.io/secret-type: repository
-stringData:
-  type: git
-  url: ${GIT_URI}
-  password: ${GH_PASS}
-  username: ${GH_USER}
-EOF
+echo -e "\nYour ArgoCD Admin user password is "
+kubectl config set-context --current --namespace=argocd --cluster=k3d-demo
+kubectl -n argocd get secret argocd-initial-admin-secret -o jsonpath="{.data.password}" | base64 -d
+echo
