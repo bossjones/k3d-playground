d82b5f5afb3cb86c658fb5ac33c4d1dd36fd2630
Malcolm Jones <bossjones@theblacktonystark.com>
2024-01-25 17:22:55 -0500

    wip: updae audit log config

--- scripts/gen-serviceaccount-rbac.sh
+++ scripts/gen-serviceaccount-rbac.sh
@@ -8,3 +8,16 @@ kubectl get serviceaccounts --all-namespaces --output=custom-columns="NAMESPACE:
 # Produce ENV for all pods, assuming you have a default container for the pods, default namespace and the `env` command is supported.
 # Helpful when running any supported command across all pods, not just `env`
 for sa in $(kubectl get serviceaccounts --all-namespaces --output=custom-columns="NAMESPACE:.metadata.namespace,NAME:.metadata.name" | awk '{ print $1":"$2 }'); do echo "$sa" && audit2rbac -f audit/logs/audit.log --serviceaccount="$sa" --generate-labels="" --generate-annotations="" --generate-name=$(echo "${sa}" | cut -d":" -f2) >generated/sa-$(echo "${sa}" | cut -d":" -f1)-$(echo "${sa}" | cut -d":" -f2).yaml; done
+
+# Specify the directory you want to check
+a_directory="./generated"
+
+# Loop through files in the a_directory
+for afile in "$a_directory"/*; do
+  # Check if the file is empty
+  if [ ! -s "$afile" ]; then
+    echo "$afile is empty. nuking..."
+    cat "$afile"
+    rm -vf $afile
+  fi
+done
