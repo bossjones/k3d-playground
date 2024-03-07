f178948b264db9dcadf2787336f81ed7700bdc91
Malcolm Jones <bossjones@theblacktonystark.com>
2024-03-07 16:23:44 -0500

    chg: fix argocd

--- scripts/deploy-external-secrets.sh
+++ scripts/deploy-external-secrets.sh
@@ -3,7 +3,7 @@
 set -x
 
 kustomize build --enable-alpha-plugins --enable-exec apps/argocd/base/kube-system/external-secrets | kubectl apply --server-side -f -
-echo "waiting for external-secrets"
-kubectl -n kube-system wait deployment external-secrets-cert-controller --for condition=Available=True --timeout=300s
-kubectl -n kube-system wait deployment external-secrets-webhook --for condition=Available=True --timeout=300s
-kubectl -n kube-system wait deployment external-secrets --for condition=Available=True --timeout=300s
+# echo "waiting for external-secrets"
+# kubectl -n kube-system wait deployment external-secrets-cert-controller --for condition=Available=True --timeout=300s
+# kubectl -n kube-system wait deployment external-secrets-webhook --for condition=Available=True --timeout=300s
+# kubectl -n kube-system wait deployment external-secrets --for condition=Available=True --timeout=300s
