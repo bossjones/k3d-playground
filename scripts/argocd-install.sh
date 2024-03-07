fde50b3a3497f04b65a89225019ce821cd7ad5d2
Malcolm Jones <bossjones@theblacktonystark.com>
2024-03-07 16:02:51 -0500

    chg: fix argocd

--- scripts/argocd-install.sh
+++ scripts/argocd-install.sh
@@ -4,8 +4,6 @@ set -x
 
 kubectx k3d-demo
 
-kubectl -n kube-system apply --server-side -f https://raw.githubusercontent.com/external-secrets/external-secrets/v0.9.11/deploy/crds/bundle.yaml || true
-retry -t 4  -- kubectl -n kube-system apply --server-side -f apps/argocd/base/kube-system/external-secrets/app/connect/clusterStore.yaml
 
 # cd apps/argocd
 kubectl create namespace argocd 2>/dev/null || true
@@ -16,6 +14,9 @@ kustomize build --enable-alpha-plugins --enable-exec apps/argocd | kubectl apply
 kubectl wait deploy/argocd-server -n argocd --for condition=available --timeout=600s
 echo ""
 # cd -
+
+kubectl -n kube-system apply --server-side -f https://raw.githubusercontent.com/external-secrets/external-secrets/v0.9.11/deploy/crds/bundle.yaml || true
+retry -t 4  -- kubectl -n kube-system apply --server-side -f apps/argocd/base/kube-system/external-secrets/app/connect/clusterStore.yaml
 
 sleep 30
 
