528743bd92118db069398d9f8068228bee37f713
Malcolm Jones <bossjones@theblacktonystark.com>
2024-03-14 18:46:28 -0400

    fix: delete-all-docker

--- scripts/delete-all-docker.sh
+++ scripts/delete-all-docker.sh
@@ -8,17 +8,17 @@ echo "# parent path -------------->  ${0%/*}  "
 echo ""# my "name ------------------>  ${0##*/} "
 echo ""
 
-
-set -euxo pipefail
+set -x
+# set -euxo pipefail
 
 # Stop the container(s) using the following command:
-docker-compose down
+docker-compose down || true
 
 # Delete all containers using the following command:
-docker rm -f $(docker ps -a -q)
+docker rm -f $(docker ps -a -q) || true
 
 # Delete all volumes using the following command:
-docker volume rm $(docker volume ls -q)
+docker volume rm $(docker volume ls -q) || true
 
-# Restart the containers using the following command:
-docker-compose up -d
+# # Restart the containers using the following command:
+# docker-compose up -d
